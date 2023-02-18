147b08c60d12abbed292e03fd6b45a933118dfe3
yossefomar <yossefomar931@gmail.com>
2023-02-18 08:16:26 -0800

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print the alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; ++c)
+	{
+		putchar(c);
+	}
+	return (0);
+}
