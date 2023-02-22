eafcdfb03e6b9d2fb8812e6a8e1f6f6545384dac
yossefomar <yossefomar931@gmail.com>
2023-02-22 15:03:46 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - get the absolute value of a number
+ *
+ * @n: integere number
+ *
+ * Return: the absolute value of the number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
