16d0957fd2271f0203b5cf5255c11d573b05b9f6
yossefomar <yossefomar931@gmail.com>
2023-02-22 14:47:06 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *print sign of number
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int print_sign(int n)
+	{
+		if (n > 0)
+		{
+			_putchar('+');
+			return (1);
+		}
+		else if (n == 0)
+		{
+			_putchar('0');
+			return (0);
+		}
+		else
+		{
+			_putchar('-');
+			return (-1);
+		}
+	}
+}
