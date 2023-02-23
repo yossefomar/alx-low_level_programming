d9e92ece8c7bf7c8ba91b2fc4d81e43451ac97fb
yossefomar <yossefomar931@gmail.com>
2023-02-22 16:03:54 -0800

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,8 @@ void jack_bauer(void)
 			_putchar(hour / 10 + '0');
 			_putchar(hour % 10 + '0');
 			_putchar(':');
-			_putchar(minute / 10 + '0');
-			_putchar(minute % 10 + '0');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
 			_putchar('\n');
 		}
 	}
