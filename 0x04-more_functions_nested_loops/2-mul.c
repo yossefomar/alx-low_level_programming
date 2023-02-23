48ffdf639e613082fd91c863f87f71cb14712ed4
yossefomar <yossefomar931@gmail.com>
2023-02-23 15:31:01 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - check if it's alpha or not
+ *
+ * @a: the first integer
+ *
+ * @b: the second integer
+ *
+ * Return: 1 -> alpha | 0 -> other wise
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
