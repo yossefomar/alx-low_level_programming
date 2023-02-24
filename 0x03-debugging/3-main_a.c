e52313ba96c9a15443c8b560e550707cb215956e
yossefomar <yossefomar931@gmail.com>
2023-02-24 15:50:48 -0800

    1

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
+* main - causes an infinite loop
 * Return: 0
 */
 
 int main(void)
 {
-    int month;
-    int day;
-    int year;
+	int i;
 
-    month = 4;
-    day = 01;
-    year = 1997;
+	printf("Infinite loop incoming :(\n");
 
-    printf("Date: %02d/%02d/%04d\n", month, day, year);
+	i = 0;
+	/*
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
 
-    day = convert_day(month, day);
-
-    print_remaining_days(month, day, year);
-
-    return (0);
+	return (0);
 }
