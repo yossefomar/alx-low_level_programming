ce4654923f21f903c33e833336d5c4ca9a8aecab
yossefomar <yossefomar931@gmail.com>
2023-02-23 16:09:53 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,19 @@
 void more_numbers(void)
 {
 	int i, j;
-
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
+			char c = j + '0';
+			if (j > 9)
+			{
+				c = (j % 10) + '0';
+				_putchar('1');
+			}
+			_putchar(c);
 		}
 		_putchar('\n');
 	}
+	return (0);
 }
