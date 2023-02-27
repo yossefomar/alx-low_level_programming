306fa2487d4a1640eea49e884fd71e82e4b2cb26
yossefomar <yossefomar931@gmail.com>
2023-02-27 13:57:27 -0800

    len of str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - return len of str
+ *
+ * @s: pointer of first element of string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
