c87b60f21d545bcdb9bee01989dc301780ce4a96
yossefomar <yossefomar931@gmail.com>
2023-02-28 08:53:52 -0800

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _atoi - that print gavin str by put
+ *
+ * @s: gavin str
+ *
+ * Return: num -> number in s
+ */
+int _atoi(char *s)
+{
+	int num;
+
+	num = atoi(s);
+	return (num);
+}
