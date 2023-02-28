14ea492ad896a796ee9840bbfb24904d419e888a
yossefomar <yossefomar931@gmail.com>
2023-02-28 09:44:35 -0800

    ranpass

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define PASSWORD_LENGTH 12
+/**
+ * main - that print gavin str by put
+ *
+ * Return: succsess(0)
+ */
+
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1] = {0};
+
+	int i;
+
+	srand(time(NULL));
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		switch (rand() % 3)
+		{
+			case 0:
+				password[i] = rand() % 10 + '0';
+				break;
+			case 1:
+				password[i] = rand() % 26 + 'a';
+				break;
+			case 2:
+				password[i] = rand() % 26 + 'A';
+				break;
+		}
+	}
+	printf("%s\n", password);
+}
