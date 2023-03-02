08ae1ec419a4379a99359d2969878bda236bc02e
yossefomar <yossefomar931@gmail.com>
2023-03-02 10:08:10 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - function concat with some conds.
+ *
+ * @s1: first str
+ *
+ * @s1: second str
+ *
+ * Return: ans-> str after concat
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(dest, src));
+}
