a304d452c0815aef80df47e667c8dfe911356278
yossefomar <yossefomar931@gmail.com>
2023-03-02 10:01:41 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncat - function concat with some conds.
+ *
+ * @dest: first str
+ *
+ * @src: second str
+ *
+ * @n: max number to be represented
+ *
+ * Return: dest-> str after concat
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
