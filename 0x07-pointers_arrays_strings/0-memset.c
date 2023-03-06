3e142627ff91f324d4fa7f93fee80e8299cb4ac9
yossefomar <yossefomar931@gmail.com>
2023-03-06 06:23:33 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memset - fill memory firtst n bytes by s pointed by p
+ *
+ * @b: the constant will filled 
+ *
+ * @s: pointer of b
+ *
+ * @m: number of bits will be used
+ */
+#include <string.h>
+
+char* memset(char* s, char b, unsigned int n) {
+    return ((char*) memset(s, (int) b, (size_t) n));
+}
+
