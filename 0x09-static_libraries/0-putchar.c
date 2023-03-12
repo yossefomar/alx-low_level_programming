15ece274ac657f7df2ec2da303cbf3be4879482f
Youssef-Omar <yossefomar931@gmail.com>
2023-03-12 02:03:28 +0200

    adding files

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,17 +1,4 @@
-#include "main.h"
-/**
- * main - Entry point
- *print call put char function
- * Return: Always 0 (Success)
- */
-int main(void)
+int _putchar(char c)
 {
-	char message[] = "_putchar\n";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(message[i]);
-	}
-	return (0);
+	return (c);
 }
